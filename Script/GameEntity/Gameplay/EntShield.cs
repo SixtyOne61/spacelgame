48fc406ce58bbfadf50b7ca9654a8d466843e5ff
SixtyOne61 <olivier.lavalley@gmail.com>
2019-05-19 21:02:09 +0200

    redo entity shield

--- Script/GameEntity/Gameplay/EntShield.cs
+++ Script/GameEntity/Gameplay/EntShield.cs
@@ -5,7 +5,7 @@ using Engine;
 using Tool;
 
 //[ExecuteInEditMode]
-public class EntShield : VolumeEntity
+public class EntShield : VisuelEntity
 {
     public CompShield ComponentShield;
     // max and min in each axis
@@ -16,24 +16,31 @@ public class EntShield : VolumeEntity
     [HideInInspector]
     public Vector2Int Z;
 
+    // for build shield
+    private List<Vector3> _vertices = new List<Vector3>();
+    private List<int> _triangles = new List<int>();
+
     public override void Start()
     {
-    	// compute vertex
-    	InitVertex();
+        // TO DO : create mesh with the coord without volume entity
+        //CompMeshGenerator.ParamCubeSize = ScriptableObject.CreateInstance(typeof(Tool.SCROneValue)) as Tool.SCROneValue;
+        //CompMeshGenerator.ParamCubeSize.Value = 1.0f;
+        // compute vertex
+        InitVertex();
         AddComponent(ComponentShield);
         base.Start();
     }
     
     public void InitVertex()
     {
-    	LinkPosList.Add(new LinkPos(new UnitPos(X.x, Y.x, Z.x)));
-    	LinkPosList.Add(new LinkPos(new UnitPos(X.y, Y.x, Z.x)));
-    	LinkPosList.Add(new LinkPos(new UnitPos(X.y, Y.y, Z.x)));
-    	LinkPosList.Add(new LinkPos(new UnitPos(X.x, Y.y, Z.x)));
-    	
-    	LinkPosList.Add(new LinkPos(new UnitPos(X.x, Y.x, Z.y)));
-    	LinkPosList.Add(new LinkPos(new UnitPos(X.y, Y.x, Z.y)));
-    	LinkPosList.Add(new LinkPos(new UnitPos(X.y, Y.y, Z.y)));
-    	LinkPosList.Add(new LinkPos(new UnitPos(X.x, Y.y, Z.y)));	
+    	//LinkPosList.Add(new LinkPos(new UnitPos(X.x, Y.x, Z.x)));
+    	//LinkPosList.Add(new LinkPos(new UnitPos(X.y, Y.x, Z.x)));
+    	//LinkPosList.Add(new LinkPos(new UnitPos(X.y, Y.y, Z.x)));
+    	//LinkPosList.Add(new LinkPos(new UnitPos(X.x, Y.y, Z.x)));
+    	//
+    	//LinkPosList.Add(new LinkPos(new UnitPos(X.x, Y.x, Z.y)));
+    	//LinkPosList.Add(new LinkPos(new UnitPos(X.y, Y.x, Z.y)));
+    	//LinkPosList.Add(new LinkPos(new UnitPos(X.y, Y.y, Z.y)));
+    	//LinkPosList.Add(new LinkPos(new UnitPos(X.x, Y.y, Z.y)));	
     }
 }
