a31fb043c77e4916e5af271c3a421954ce225486
SixtyOne61 <olivier.lavalley@gmail.com>
2019-07-10 14:26:03 +0200

    start init bbbase

--- Script/Engine/Collision/BBBase.cs
+++ Script/Engine/Collision/BBBase.cs
@@ -6,6 +6,38 @@ namespace Engine
 {
     public class BBBase
     {
+        private float _sizeCube = 0.0f;
 
+        // list of box position
+        [HideInInspector]
+        public float xMin = 0.0f;
+        [HideInInspector]
+        public float xMax = 0.0f;
+        [HideInInspector]
+        public float yMin = 0.0f;
+        [HideInInspector]
+        public float yMax = 0.0f;
+        [HideInInspector]
+        public float zMin = 0.0f;
+        [HideInInspector]
+        public float zMax = 0.0f;
+
+        BBBase()
+        {
+
+        }
+
+        BBBase(LinkPos init, float sizeCube)
+        {
+            _sizeCube = sizeCube;
+            xMin = init.Center.x - _sizeCube;
+            xMax = init.Center.x + _sizeCube;
+
+            yMin = init.Center.y - _sizeCube;
+            yMax = init.Center.y + _sizeCube;
+
+            zMin = init.Center.z - _sizeCube;
+            zMax = init.Center.z + _sizeCube;
+        }
     }
 }
