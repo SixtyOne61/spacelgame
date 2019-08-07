fb36034f49733a17a0d6df3f84526659a6e04b7b
SixtyOne61 <olivier.lavalley@gmail.com>
2019-08-07 23:13:59 +0200

    add contacts

--- Script/Engine/Entity/CollideEntity.cs
+++ Script/Engine/Entity/CollideEntity.cs
@@ -56,6 +56,9 @@ namespace Engine
             {
                 return;
             }
+
+            ContactPoint[] points = new ContactPoint[other.contactCount];
+            other.GetContacts(points);
         }
 
         public void OnTriggerStay(Collider other)
